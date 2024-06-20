4aa024449fde4b54117a31e68ce62da980a4642f
smilesnail <app_!QAZxsw20>
2024-06-20 23:12:59 +0800

    开发区块功能

---
+++ src/components/Logo/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="logo" @click="toHome">
+    <a-image width="40px" class="logo-image" :src="logo"/>
+    <span class="title">
+      {{ settingConfig.title }}
+    </span>
+  </div>
+</template>
+<script setup>
+import logo from '@/assets/logo/logo.png'
+import {settingConfig} from '@/config/setting.config.ts'
+import {useRouter} from 'vue-router'
+
+const router = useRouter()
+
+const emit = defineEmits(null)
+
+const toHome = () => {
+  router.push('/')
+  emit('removeMenuActive')
+  scrollTo(0, 800)
+}
+</script>
+<style lang="scss" scoped>
+.logo {
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+
+  .logo-image {
+    line-height: 100%;
+    border-radius: 50%;
+    vertical-align: middle;
+  }
+
+  .title {
+    margin-left: 10px;
+    font-size: 22px;
+    background: linear-gradient(to right, #fea90b, #fab301, #fed60b);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    font-weight: bold;
+    vertical-align: middle;
+  }
+}
+</style>
---
+++ src/views/home/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <div class="contain">
+      <p class="title">开启您的区块链学习旅途!</p>
+      <a-card class="r-card" title="SHA256">
+        <template #extra>
+          <a-button type="primary" @click="doSubmit">确认</a-button>
+        </template>
+        <a-form :labelCol="{ span: 3 }" :wrapperCol="{ span: 22 }" class="r-form">
+          <a-form-item label="数据">
+            <a-textarea :rows="6" v-model:value="data"/>
+          </a-form-item>
+          <a-form-item label="哈希">
+            <a-input disabled v-model:value="hash"/>
+          </a-form-item>
+        </a-form>
+      </a-card>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import {ref} from "vue";
+import CryptoJS from "crypto-js"
+
+
+const hash = ref("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855")
+
+const data = ref()
+const doSubmit = () => hash.value = CryptoJS.SHA256(data.value).toString();
+</script>
+<style lang="scss">
+.container {
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .contain {
+    margin-top: 15vh;
+
+    .title {
+      text-align: center;
+      font-size: 35px;
+      font-weight: bold
+    }
+
+    .r-card {
+      margin-top: 10vh;
+      min-width: 750px;
+
+      .r-form {
+        padding: 10px 30px;
+      }
+    }
+  }
+
+
+}
+</style>
